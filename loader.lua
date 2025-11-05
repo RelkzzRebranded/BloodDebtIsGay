5cec12d4a10db13cf6f71a1ff583a2e0a91e6046
RelkzzRebranded <130754062+RelkzzRebranded@users.noreply.github.com>
2025-11-05 21:28:21 +0800

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,27 @@
+local actors = getactors()
+local function CheckFFlagValue(Name, Value)
+	local Success, Result = pcall(getfflag, Name)
+	if not Success then
+		return false
+	end
+
+	if typeof(Result) == "boolean" then
+		return Result
+	end
+
+	if typeof(Result) == "string" then
+		return Result == tostring(Value)
+	end
+
+	return false
+end
+local function Load()
+  return loadstring(game:HttpGet("https://raw.githubusercontent.com/RelkzzRebranded/BloodDebtIsGay/refs/heads/main/core.lua"))()
+end
+if CheckFFlagValue("DebugRunParallelLuaOnMainThread", "True") then
+  Load()
+else
+  run_on_actor(actors[1], [=[
+    Load()
+  ]=])
+end
