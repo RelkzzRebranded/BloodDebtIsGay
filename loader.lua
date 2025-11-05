dc973a654f0a20f868c5efbc4f0b73303cb69150
RelkzzRebranded <130754062+RelkzzRebranded@users.noreply.github.com>
2025-11-05 21:37:22 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -18,7 +18,7 @@ end
 local function Load()
   return loadstring(game:HttpGet("https://raw.githubusercontent.com/RelkzzRebranded/BloodDebtIsGay/refs/heads/main/core.lua"))()
 end
-if CheckFFlagValue("DebugRunParallelLuaOnMainThread", "True") then
+if CheckFFlagValue("DebugRunParallelLuaOnMainThread", true) then
   Load()
 else
   run_on_actor(actors[1], [=[
